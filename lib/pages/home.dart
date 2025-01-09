0da4dd1c270c9919a56eedc7a79aea880e6cfc59
Joe Ulyatt <joewhocodes@gmail.com>
2025-01-09 14:48:21 +0100

    feat: :sparkles: Add Plus Button to home Nav

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,6 +9,38 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home'),
         centerTitle: false,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Container(
+              width: 36.0,
+              height: 36.0,
+              decoration: BoxDecoration(
+                color: Color(0xFF3C3D3E),
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withValues(alpha: 0.3),
+                    offset: Offset(0, 2),
+                    blurRadius: 4,
+                  ),
+                ],
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  print('Plus icon pressed');
+                },
+                child: Center(
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.white,
+                    size: 22.0,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
